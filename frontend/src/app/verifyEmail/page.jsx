04a63b506cf0b7644ef2591d97fcdc46ad3b3b10
uday chaudhary <udaychaudhary@udays-MacBook-Air.local>
2025-08-30 17:47:20 +0530

    update vercel issue

--- frontend/src/app/verifyEmail/page.jsx
+++ frontend/src/app/verifyEmail/page.jsx
@@ -1,46 +1,11 @@
-"use client";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useSearchParams, useRouter } from "next/navigation";
-
+import VerifyEmailContent from "./../../components/verifyContent";
+import { Suspense } from "react";
 export default function VerifyEmailPage() {
-  const [message, setMessage] = useState("Verifying your email...");
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  useEffect(() => {
-    const verifyToken = async () => {
-      const token = searchParams.get("token");              //token for verification 
-
-      if (!token) {
-        setMessage("Invalid or missing token.");
-        return;
-      }
-
-      try {
-        const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/verify-email?token=${token}`
-        );
-        if (response.status === 200) {
-          setMessage("✅ Email verified successfully. Redirecting to login...");
-          setTimeout(() => {
-            router.push("/login");
-          }, 3000);
-        }
-      } catch (err) {
-        console.error(err);
-        setMessage("Verification failed. The token might be invalid or expired.");
-      }
-    };
-
-    verifyToken();
-  }, [searchParams, router]);
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#efebe0] px-4">
-      <div className="max-w-md p-8 border rounded-lg shadow text-center">
-        <h2 className="text-xl font-semibold text-green-500">{message}</h2>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <VerifyEmailContent />
+      </Suspense>
     </div>
   );
 }
